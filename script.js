610c355828688e675494e49827e35de16aa331a8
tej-gnp <tajfire97@gmail.com>
2025-05-30 15:56:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
-
-gsap.from("#headline", {
-  duration: 1,
-  y: -50,
-  opacity: 0,
-  ease: "power2.out"
-});
-gsap.from(".cta-button", {
-  duration: 1,
-  delay: 0.5,
-  y: 20,
-  opacity: 0,
-  ease: "power2.out"
-});
+gsap.from("#headline", {duration: 1, y: -50, opacity: 0, ease: "power2.out"});
+gsap.from(".cta-button", {duration: 1, delay: 0.5, y: 20, opacity: 0, ease: "power2.out"});
