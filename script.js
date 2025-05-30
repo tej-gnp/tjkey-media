8340044a2a989e470bc414d70587482973d4c129
tej-gnp <tajfire97@gmail.com>
2025-05-30 15:38:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+gsap.from("#headline", {
+  duration: 1,
+  y: -50,
+  opacity: 0,
+  ease: "power2.out"
+});
+gsap.from(".cta-button", {
+  duration: 1,
+  delay: 0.5,
+  y: 20,
+  opacity: 0,
+  ease: "power2.out"
+});
